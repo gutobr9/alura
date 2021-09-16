60d05107988c8cba1da28994626e5e7c02ce62aa
Gustavo B Rodrigues <gustavo.bezerrarodrigues@gmail.com>
2021-09-16 00:46:57 -0300

    diferença entre método str() e repr()

--- python/teste/programa.py
+++ python/teste/programa.py
@@ -1,61 +1,70 @@
-# filme duracao
-# serie temporadas
-
 class Programa:
     def __init__(self,nome,ano):
-        self.__nome = nome
-        self.__ano = ano
-        self.__likes = 0
+        self._nome = nome
+        self._ano = ano
+        self._likes = 0
 
     @property
     def likes(self):
-        return self.__likes
-    
+        return self._likes
+
     def dar_like(self):
-        self.__likes += 1
+        self._likes += 1
 
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
     
     @nome.setter
     def nome(self,nome):
-        self.__nome = nome
+        self._nome = nome
 
     @property
     def ano(self):
-        return self.__ano
-
+        return self._ano
+    
     @ano.setter
     def ano(self,ano):
-        self.__ano = ano
-
+        self._ano = ano
 
 class Filme(Programa):
-    def __init__(self,nome,ano,duracao):
-        self.nome = nome
-        self.ano = ano
-        self.likes = 0
-        self.__duracao = duracao
+    def __init__(self,nome,ano,duracao): 
+        super().__init__(nome,ano)
+        self.duracao = duracao
 
-    @property
-    def duracao(self):
-        return self.__duracao
-    
-    @duracao.setter
-    def duracao(self,duracao):
-        self.__duracao = duracao
+class Serie(Programa): 
+    def __init__(self,nome,ano,temporadas):
+        super().__init__(nome,ano)
+        self.temporadas = temporadas
 
-# p = Programa("Up",2005)
-
-# for i in range(5):
-#     p.dar_like()
-
+p = Programa("Gustavo",1994)
+print(p)
+repr(p)
+# p.dar_like()
 # print(f"{p.nome} {p.ano} {p.likes}")
 
-f = Filme("O Auto da Compadecida",1999,160)
+# print("====================")
 
-for i in range(10):
-    f.dar_like()
+vingadores = Filme("vingadores - guerra infinita",2018,160)
+vingadores.dar_like()
+vingadores.dar_like()
+# print(f"{vingadores.nome} {vingadores.ano} {vingadores.duracao}")
+# print(f" {vingadores.likes}")
+print("====================")
 
-print(f"filme {f.nome} - ano {f.ano} - duracao {f.duracao} ")
+atlanta = Serie("atlanta",2018,2)
+atlanta.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
+# print(f"{atlanta.nome} {atlanta.ano} {atlanta.temporadas}")
+# print(f" {atlanta.likes}")
+
+filmes_e_series = [vingadores,atlanta]
+
+print("====================")
+for programa in filmes_e_series:
+    detalhes = str(programa.duracao)+" min" if hasattr(programa,"duracao") else str(programa.temporadas)+" temporadas"
+    print(f"{programa.nome} {detalhes} - likes: {programa.likes}")
+    repr(f"{programa.nome} {detalhes} - likes: {programa.likes}")
+    print(programa)
+    repr(programa)
